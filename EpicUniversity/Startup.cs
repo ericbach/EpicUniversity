c83a9cdf0cf1c6799421ef90dea5d7deb82d207e
Eric Bach <eric.bach@ama.ab.ca>
2020-12-22 16:34:35 -0700

    Add initial seeding

--- EpicUniversity/Startup.cs
+++ EpicUniversity/Startup.cs
@@ -1,16 +1,12 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using EpicUniversity.Data;
+using EpicUniversity.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EpicUniversity
@@ -40,6 +36,9 @@ namespace EpicUniversity
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                // Seed database
+                app.MigrateAndSeedData();
             }
 
             app.UseHttpsRedirection();
@@ -52,6 +51,34 @@ namespace EpicUniversity
             {
                 endpoints.MapControllers();
             });
+        }
+    }
+
+    public static class UniversityContextMigrateAndSeed
+    {
+        public static void MigrateAndSeedData(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            using var context = serviceScope.ServiceProvider.GetService<UniversityContext>();
+
+            // Ensure database is created
+            context.Database.EnsureCreated();
+
+            // Ensure any pending migrations are applied
+            if (context.Database.GetPendingMigrations().Any())
+                context.Database.Migrate();
+
+            // Seed test data in database
+            var course = new Course
+            {
+                CreatedDate = DateTime.Today,
+                Name = "Epic programming",
+                Credits = 2
+            };
+            
+            // EF
+            context.Courses.Add(course);
+            context.SaveChanges();
         }
     }
 }
