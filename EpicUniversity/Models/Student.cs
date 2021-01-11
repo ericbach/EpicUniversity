b5e94c480060add58c41ff4b4b89a379595cc73b
Eric Bach <eric.bach@ama.ab.ca>
2021-01-11 16:06:34 -0700

    Split up Personnel file

--- EpicUniversity/Models/Student.cs
+++ EpicUniversity/Models/Student.cs
@@ -12,14 +12,4 @@ namespace EpicUniversity.Models
         public IList<Course> Courses { get; set; } = new List<Course>();
         public IList<Grade> Grades { get; set; } = new List<Grade>();
     }
-
-    public class Professor : Entity
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime Birthdate { get; set; }
-        public int Tenure { get; set; }
-        public int ParkingSpot { get; set; }
-        public IList<Course> Courses { get; set; } = new List<Course>();
-    }
 }
