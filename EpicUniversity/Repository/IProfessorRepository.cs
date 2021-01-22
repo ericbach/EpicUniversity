b1981d847fb752dc4fe029d6ecdd97436174d347
Sejal Panchal <Sejal.Panchal@ama.ab.ca>
2021-01-21 17:46:43 -0700

    controller and repo

---
+++ EpicUniversity/Repository/IProfessorRepository.cs
@@ -0,0 +1,9 @@
+﻿using EpicUniversity.Models;
+
+namespace EpicUniversity.Repository
+{
+    public interface IProfessorRepository:IRepository<Professor>
+    {
+        Professor GetProfessorWithCourseInfo(long id);
+    }
+}
